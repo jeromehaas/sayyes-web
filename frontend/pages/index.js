ade935ce1d78dd8475faa51e44010f0a21cf5265
jeromehaas <hello@jeromehaas.ch>
2023-03-27 09:52:15 +0200

    Setup: heading component

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,10 +1,14 @@
 import Section from "components//layouts/section/section";
+import Heading from "components//content/heading/heading";
 
 const Start = ( ) => {
 
   return (
     <Section className="start">
-      Hello World!
+      <Heading level="h1">Hello World</Heading>
+      <Heading level="h2">Hello World</Heading>
+      <Heading level="h3">Hello World</Heading>
+      <Heading level="h4">Hello World</Heading>
     </Section>
   );
 
