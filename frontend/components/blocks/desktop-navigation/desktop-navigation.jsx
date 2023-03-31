56a0324b32866bb7d65ae685891efca6484aaea8
jeromehaas <hello@jeromehaas.ch>
2023-03-31 10:36:03 +0200

    Refactor: desktop-navigation to show active route

--- frontend/components/blocks/desktop-navigation/desktop-navigation.jsx
+++ frontend/components/blocks/desktop-navigation/desktop-navigation.jsx
@@ -1,7 +1,11 @@
 import Link from "next/link";
 import { useEffect, useState} from "react";
+import { useRouter } from "next/router";
 
 const DesktopNavigation = () => {
+
+	// BRING IN ROUTER
+	const router = useRouter();
 
 	// SETUP STATE TO TRACK SCROLL POSITION
 	const [scrollPosition, setScrollPosition ] = useState(0);
@@ -21,11 +25,11 @@ const DesktopNavigation = () => {
 			<div className="desktop-navigation__bar bar">
 				<Link className="bar__logo-link" href="/"><img className="bar__logo" src="/logos/logo-sayyes.svg" /></Link>
 				<div className="bar__links">
-					<Link className="links__item" href="/">Home</Link>
-					<Link className="links__item" href="/buy">Kaufen</Link>
-					<Link className="links__item" href="/sell">Verkaufen</Link>
-					<Link className="links__item" href="/about">About</Link>
-					<Link className="links__item" href="/collection">Kollektion</Link>
+					<Link className={`links__item ${ router.pathname === '/' ? 'links__item--active' : '' }`} href="/">Home</Link>
+					<Link className={`links__item ${ router.pathname === '/buy' ? 'links__item--active' : '' }`} href="/buy">Kaufen</Link>
+					<Link className={`links__item ${ router.pathname === '/sell' ? 'links__item--active' : '' }`} href="/sell">Verkaufen</Link>
+					<Link className={`links__item ${ router.pathname === '/about' ? 'links__item--active' : '' }`} href="/about">About</Link>
+					<Link className={`links__item ${ router.pathname === '/collection' ? 'links__item--active' : '' }`} href="/collection">Kollektion</Link>
 				</div>
 			</div>
 			<div className="desktop-navigation__placeholder"></div>
