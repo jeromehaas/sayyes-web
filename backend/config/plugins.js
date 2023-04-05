3f90a5402dba8ff6ec17fa237984d6e58749d080
jeromehaas <hello@jeromehaas.ch>
2023-04-05 17:02:50 +0200

    Setup: nodemailer email plugin

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -10,4 +10,21 @@ module.exports = ({env}) => ({
       }
     },
   }, 
-})
+  email: {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: env('SMTP_HOST'),
+        port: env('SMTP_PORT'),
+        auth: {
+          user: env('SMTP_USERNAME'),
+          pass: env('SMTP_PASSWORD'),
+        },
+      },
+      settings: {
+        defaultFrom: 'no-reply@sayyes-bridal.ch',
+        defaultReplyTo: 'yes@ysayyes-bridal.ch',
+      },
+    },
+  },
+});
