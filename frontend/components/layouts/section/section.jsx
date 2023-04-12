1a5a6b850046d7d457e1b257216012a0f6beb519
jeromehaas <hello@jeromehaas.ch>
2023-04-12 17:54:14 +0200

    Setup: error component

--- frontend/components/layouts/section/section.jsx
+++ frontend/components/layouts/section/section.jsx
@@ -1,11 +1,13 @@
-const Section = ({ children, className }) => {
+import { forwardRef } from "react";
+
+const Section = forwardRef(({ children, className }, ref) => {
 
     return (
-        <section className={`${ className } section`}>
+        <section className={`${ className } section`} ref={ ref }>
             <div className={`${ className }__inner section__inner`}>{ children }</div>
         </section>
     )
 
-};
+});
 
 export default Section;
---
+++ frontend/pages/404.js
@@ -0,0 +1,11 @@
+ import Error from "components/sections/error/error";
+
+const PageNotFound = () => {
+
+    return (
+        <Error />
+    )
+
+};
+
+export default PageNotFound;
