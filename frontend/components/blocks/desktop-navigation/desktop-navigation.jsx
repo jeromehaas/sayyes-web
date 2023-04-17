1d8eada94a1cf42bb74601e8a027e9bbbd6fe07a
Jérôme Haas <hello@jeromehaas.ch>
2023-04-18 00:34:01 +0200

    Refactor: use gsap animations for desktop-navigation

--- frontend/components/blocks/desktop-navigation/desktop-navigation.jsx
+++ frontend/components/blocks/desktop-navigation/desktop-navigation.jsx
@@ -1,28 +1,42 @@
 import Link from "next/link";
-import { useEffect, useState} from "react";
+import { useEffect, useState, useRef } from "react";
 import { useRouter } from "next/router";
+import { gsap } from "gsap";
 
 const DesktopNavigation = () => {
 
 	// BRING IN ROUTER
 	const router = useRouter();
 
-	// SETUP STATE TO TRACK SCROLL POSITION
-	const [scrollPosition, setScrollPosition ] = useState(0);
+	// SETUP REFS
+	const desktopNavigationRef = useRef();
 
 	// HANDLE SCROLL
 	const handleScroll = () => {
-		setScrollPosition(window.scrollY);
+		if (window.scrollY > 10) {
+			gsap.context(() => {
+				gsap.to('.desktop-navigation .desktop-navigation__bar', { height: '120px', duration: 0.05, ease: 'power4.inOut' })	
+				gsap.to('.desktop-navigation .desktop-navigation__placeholder', { height: '120px', duration: 0.05, ease: 'power4.inOut' })	
+				gsap.to('.desktop-navigation .bar__logo', { marginTop: '0px', marginBottom: '24px', height: '24px', duration: 0.05, ease: 'power4.inOut'})	
+			}, desktopNavigationRef);
+		} else {
+			gsap.context(() => {
+				gsap.to('.desktop-navigation .desktop-navigation__bar', { height: '200px', duration: 0.05, ease: 'power4.inOut' })	
+				gsap.to('.desktop-navigation .desktop-navigation__placeholder', { height: '200px', duration: 0.05, ease: 'power4.inOut' })	
+				gsap.to('.desktop-navigation .bar__logo', { marginTop: '40px', marginBottom: '40px', height: '40px', duration: 0.05, ease: 'power4.inOut'})	
+			}, desktopNavigationRef);
+		}
 	};
 
 	// ADD EVENTLISTENER ON FIRST RENDER
 	useEffect(() => {
 		window.addEventListener('scroll', handleScroll);
+		return () => window.removeEventListener('scroll', handleScroll);
 	}, [])
 
 	return (
-		<nav className={`desktop-navigation ${ scrollPosition > 10 ? 'desktop-navigation--small' : ''}`}>
-			<div className="desktop-navigation__bar bar">
+		<nav className="desktop-navigation" ref={ desktopNavigationRef }>
+				<div className="desktop-navigation__bar bar">
 				<Link className="bar__logo-link" href="/"><img className="bar__logo" src="/logos/logo-sayyes.svg" /></Link>
 				<div className="bar__links">
 					<Link className={`links__item ${ router.pathname === '/' ? 'links__item--active' : '' }`} href="/">Home</Link>
