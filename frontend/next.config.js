2a1dfcbf2f6d75fdc5ab5635dfa57aeb826b4463
jeromehaas <hello@jeromehaas.ch>
2023-05-31 10:03:25 +0200

    Refactor: update nex config file

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,10 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: false,
 	images: {
 		domains: ['localhost', 'picsum.photos', 'sayyes-web-development.fra1.digitaloceanspaces.com', 'sayyes-web-production.fra1.digitaloceanspaces.com'],
+	},
+	eslint: {
+		ignoreDuringBuilds: true,
 	},
 };
 
