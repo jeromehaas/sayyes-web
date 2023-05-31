bf80e2398d560b59ab289d42757d4c8dfa824a16
jeromehaas <hello@jeromehaas.ch>
2023-05-31 09:13:43 +0200

    Setup: cors configuration

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -19,7 +19,15 @@ module.exports = [
 			},
 		},
 	},
-	'strapi::cors',
+	{
+		name: 'strapi::cors',
+		config: {
+			origin: ['*'],
+			methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+			headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+			keepHeaderOnError: true,
+		},
+	},
 	'strapi::poweredBy',
 	'strapi::logger',
 	'strapi::query',
