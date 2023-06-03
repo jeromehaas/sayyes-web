58b811c993d86e8f39c0efb4ede4c04dda3f84e7
jeromehaas <hello@jeromehaas.ch>
2023-06-03 22:16:41 +0200

    Refactor: add correct syntax for multiple choices in file input

--- frontend/components/inputs/input-file/input-file.jsx
+++ frontend/components/inputs/input-file/input-file.jsx
@@ -6,7 +6,7 @@ const InputFile = ({ className, id, label, register, header = 'test', errors, er
 	return (
 		<div className={ `${ className } input-file input-field` }>
 			<label className="input-file__label input-field__label">{ label }</label>
-			<input className="input-file__fake-input input-field__fake-input" type="file" id="file" name="file" filename={ filename } { ...register(id, validation) } multiple="multiple" readOnly />
+			<input className="input-file__fake-input input-field__fake-input" type="file" id="file" name="file" filename={ filename } { ...register(id, validation) } multiple readOnly />
 			<div className="input-file__input-container input-field__input-container input-container">
 				<div className="input-container__header header">
 					<Paragraph className="header__text">{ value?.length ? value.length > 1 ? `${ value.length } Bilder ausgewählt` : `${ value.length } Bild ausgewählt` : header }</Paragraph>
