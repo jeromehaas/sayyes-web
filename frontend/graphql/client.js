a928b6ed4f1a5366e14c20f488f10d9c8a907178
jeromehaas <hello@jeromehaas.ch>
2023-06-04 22:31:56 +0200

    Refactor: env variable for graphql client base-url

--- frontend/graphql/client.js
+++ frontend/graphql/client.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-	uri: process.env.GRAPHQL_URI,
+	uri: process.env.NEXT_PUBLIC_GRAPHQL_URI,
 	cache: new InMemoryCache(),
 	defaultOptions: {
 		watchQuery: {
