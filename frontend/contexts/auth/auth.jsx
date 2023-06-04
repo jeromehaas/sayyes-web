fdb410318ce1ffcef945a756369bb42728a6e67e
jeromehaas <hello@jeromehaas.ch>
2023-06-04 12:03:40 +0200

    Refactor: improve logic of auth

--- frontend/contexts/auth/auth.jsx
+++ frontend/contexts/auth/auth.jsx
@@ -1,13 +1,61 @@
-import { createContext, useMemo, useState } from 'react';
+import { sha256 } from 'js-sha256';
+import { createContext, useEffect, useMemo, useState } from 'react';
+import Cookies from 'js-cookie';
 
 const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
 
+	// SETUP STATE
 	const [isAuthenticated, setIsAuthenticated] = useState(false);
+	const [isLoading, setIsLoading] = useState(true);
 
-	const value = useMemo(() => { return { isAuthenticated, setIsAuthenticated }; }, [isAuthenticated]);
+	// SETUP ROUTER
 
+	const storeToken = () => {
+		const developerLoginCode = process.env.NEXT_PUBLIC_DEVELOPER_LOGIN_CODE;
+		const token = sha256(developerLoginCode);
+		Cookies.set('token', token);
+	};
+
+	// LOGIN WITH TOKEN
+	const loginWithToken = async () => {
+		storeToken();
+		setIsAuthenticated(true);
+	};
+
+	// LOGIN WITH SESSION
+	const loginWithSession = () => {
+		setIsAuthenticated(true);
+	};
+
+	// DESTROY SESSION
+	const destroySession = () => {
+		setIsAuthenticated(false);
+	};
+
+	// CHECK LOGIN CODE
+	const checkLoginCode = (code) => {
+		if (code === process.env.NEXT_PUBLIC_DEVELOPER_LOGIN_CODE) loginWithToken();
+		if (code !== process.env.NEXT_PUBLIC_DEVELOPER_LOGIN_CODE) destroySession();
+	};
+
+	// CHECK SESSION
+	const checkSession = () => {
+		const token = Cookies.get('token');
+		if (token === sha256(process.env.NEXT_PUBLIC_DEVELOPER_LOGIN_CODE)) loginWithSession();
+		if (token !== sha256(process.env.NEXT_PUBLIC_DEVELOPER_LOGIN_CODE)) destroySession();
+	};
+
+	// GET ENVORONMENT
+	const getEnvironment = () => {
+		return process.env.NEXT_PUBLIC_ENVIRONMENT;
+	};
+
+	// MEMOIZE VALUE
+	const value = useMemo(() => { return { isAuthenticated, isLoading, setIsLoading, loginWithToken, loginWithSession, checkSession, checkLoginCode, getEnvironment, destroySession }; }, [isAuthenticated, isLoading]);
+
+	// RETURN PROVIDER
 	return (
 		<AuthContext.Provider value={ value }>
 			{ children }
--- frontend/utils/protected-routes.jsx
+++ frontend/utils/protected-routes.jsx
@@ -1,50 +1,69 @@
 import { useEffect, useContext } from 'react';
 import { useRouter } from 'next/router';
-import { getCookie } from 'cookies-next';
-import { sha256 } from 'js-sha256';
 import { AuthContext } from 'contexts/auth/auth.jsx';
 
 const ProtectedRoutes = ({ children }) => {
 
 	// BRING IN AUTH-CONTEXT
-	const { isAuthenticated, setIsAuthenticated } = useContext(AuthContext);
+	const { isAuthenticated, checkSession, isLoading, setIsLoading, getEnvironment } = useContext(AuthContext);
 
 	// SETUP ROUTER
 	const router = useRouter();
 
-	// CHECK TOKEN
-	const checkToken = (token) => {
-		return token === sha256(process.env.NEXT_PUBLIC_DEVELOPER_LOGIN_CODE);
-	};
-
-	// GET CURRENT PAHT
+	// RETURN CHILDREN IF ENVIRONMENT IS PRODUCTION
+	const environment = getEnvironment();
 	const currentPath = router.pathname;
 
-	// GET ENVIRONMENT
-	const environment = process.env.NEXT_PUBLIC_ENVIRONMENT;
+	// ON FIRST RENDER CHECK SESSION TOKEN
+	useEffect(() => {
+		checkSession();
+		setIsLoading(false);
+	}, [currentPath]);
 
-	// CHECK IF TOKEN IS VALID
-	// useEffect(() => {
-	// 	const authToken = getCookie('auth-token');
-	// 	const tokenIsValid = checkToken(authToken);
-	// 	tokenIsValid
-	// 		? setIsAuthenticated(true)
-	// 		: router.push('/login');
-	// }, [isAuthenticated]);
+	// AFTER LOADING IF USER IS NOT AUTHENTICATED REDIRECT TO LOGIN
+	useEffect(() => {
+		if (!isLoading && !isAuthenticated && environment !== 'production' && currentPath !== '/login') {
+			router.push('/login');
+		};
+	}, [isAuthenticated, isLoading]);
 
-	// RETURN CHILDREN IF ENVIRONMENT IS PRODUCTION
-	if (environment === 'production') {
+	// IF USER IS ON PRODUCTION ENVIRONMENT ON LOGIN PAGE REDIRECT TO START
+	useEffect(() => {
+		if (!isLoading && environment === 'production' && currentPath === '/login') {
+			router.push('/');
+		};
+	}, [isAuthenticated, isLoading]);
+
+	// AFTER LOADING IF USER IS AUTHENTICATED AND ON LOGIN PAGE REDIRECT TO START
+	useEffect(() => {
+		if (!isLoading && isAuthenticated && environment !== 'production' && currentPath === '/login') {
+			router.push('/');
+		};
+	}, [isAuthenticated, isLoading]);
+
+	// IF USER IS ON PRODUCTION ENVIRONMENT SHOW CONTENT
+	if (environment === 'production' && !isLoading) {
 		return children;
 	};
 
-	// RETURN CHILDREN ON LOGIN ROUTE
+	// IF USER IS AUTHENTICATED SHOW CONTENT
+	if (isAuthenticated && !isLoading) {
+		return children;
+	};
+
+	// IF USER IS ON LOGIN PAGE SHOW CONTENT
 	if (currentPath === '/login') {
 		return children;
 	};
 
-	// RETURN CHILDREN IF AUTHENTICATION IS SUCCESSFULL
-	if (isAuthenticated === true) {
+	// IF USER IS AUTHENTICATED SHOW CONTENT
+	if (isAuthenticated && !isLoading) {
 		return children;
+	};
+
+	// IF USER IS NOT AUTHENTICATED OR PAGE IS LOADING DONT SHOW CONTENT
+	if (isLoading || !isAuthenticated) {
+		return null;
 	};
 
 };
